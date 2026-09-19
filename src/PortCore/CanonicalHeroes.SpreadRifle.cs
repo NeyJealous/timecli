fbaa2f318c89f7f2321b16e362c5dfed3b159602
NeyJealous <rismahanov@yandex.ru>
2026-09-20 01:43:07 +0300

    data: reconstruct Spread Rifle progression

---
+++ src/PortCore/CanonicalHeroes.SpreadRifle.cs
@@ -0,0 +1,47 @@
+namespace TimeClickers.PortCore;
+
+public static partial class CanonicalHeroes
+{
+    private static HeroBaseSpec CreateSpreadRifle()
+    {
+        var upgrades = new UpgradeSpec[]
+        {
+            U(1, 10, (UpgradeMod.FireRate, 2.0), (UpgradeMod.HeroDps, 2.0)),
+            U(2, 25, (UpgradeMod.FireRate, 2.0), (UpgradeMod.HeroDps, 2.0)),
+            U(3, 50, (UpgradeMod.FireRate, 2.0), (UpgradeMod.Projectiles, 6.0), (UpgradeMod.HeroDps, 2.0)),
+            U(4, 75, (UpgradeMod.FireRate, 2.5), (UpgradeMod.HeroDps, 2.5)),
+            U(5, 85, (UpgradeMod.UnitedFront, 1.2)),
+            U(6, 100, (UpgradeMod.Promotion, 1.0)),
+            U(7, 110, (UpgradeMod.FireRate, 2.0), (UpgradeMod.HeroDps, 2.0)),
+            U(8, 125, (UpgradeMod.FireRate, 2.0), (UpgradeMod.HeroDps, 2.0)),
+            U(9, 150, (UpgradeMod.FireRate, 2.0), (UpgradeMod.Projectiles, 6.0), (UpgradeMod.HeroDps, 2.0)),
+            U(10, 175, (UpgradeMod.FireRate, 2.5), (UpgradeMod.HeroDps, 2.5)),
+            U(11, 185, (UpgradeMod.CriticalDamage, 3.0)),
+            U(12, 200, (UpgradeMod.Promotion, 1.0)),
+            U(13, 210, (UpgradeMod.FireRate, 2.0), (UpgradeMod.HeroDps, 2.0)),
+            U(14, 225, (UpgradeMod.FireRate, 2.0), (UpgradeMod.HeroDps, 2.0)),
+            U(15, 250, (UpgradeMod.FireRate, 2.0), (UpgradeMod.Projectiles, 6.0), (UpgradeMod.HeroDps, 2.0)),
+            U(16, 275, (UpgradeMod.FireRate, 2.5), (UpgradeMod.HeroDps, 2.5)),
+            U(17, 285, (UpgradeMod.CriticalChance, 0.02)),
+            U(18, 300, (UpgradeMod.Promotion, 1.0)),
+            U(19, 310, (UpgradeMod.FireRate, 2.0), (UpgradeMod.HeroDps, 2.0)),
+            U(20, 325, (UpgradeMod.FireRate, 2.0), (UpgradeMod.HeroDps, 2.0)),
+            U(21, 350, (UpgradeMod.FireRate, 2.0), (UpgradeMod.Projectiles, 6.0), (UpgradeMod.HeroDps, 2.0)),
+            U(22, 375, (UpgradeMod.FireRate, 2.5), (UpgradeMod.HeroDps, 2.5)),
+            U(23, 385, (UpgradeMod.GoldFind, 1.5)),
+            U(24, 400, (UpgradeMod.Promotion, 1.0)),
+            U(25, 410, (UpgradeMod.FireRate, 2.0), (UpgradeMod.HeroDps, 2.0)),
+            U(26, 425, (UpgradeMod.FireRate, 2.0), (UpgradeMod.HeroDps, 2.0)),
+            U(27, 450, (UpgradeMod.FireRate, 2.0), (UpgradeMod.Projectiles, 6.0), (UpgradeMod.HeroDps, 2.0)),
+            U(28, 475, (UpgradeMod.FireRate, 2.5), (UpgradeMod.HeroDps, 2.5)),
+            U(29, 485, (UpgradeMod.UnitedFront, 1.2)),
+            U(30, 500, (UpgradeMod.Training, 1.0)),
+            U(31, 600, (UpgradeMod.Training, 2.0), (UpgradeMod.FireRate, 2.0)),
+            U(32, 700, (UpgradeMod.Training, 3.0), (UpgradeMod.FireRate, 4.0)),
+            U(33, 800, (UpgradeMod.Training, 4.0), (UpgradeMod.FireRate, 4.0), (UpgradeMod.Projectiles, 6.0)),
+            U(34, 900, (UpgradeMod.Training, 5.0), (UpgradeMod.FireRate, 10.0), (UpgradeMod.Projectiles, 6.0)),
+            U(35, 1000, (UpgradeMod.SpecOps, 1.0)),
+        };
+        return new HeroBaseSpec(2, WeaponType.SpreadRifle, 74.0, 2.0f, 2, upgrades, "Spread Rifle");
+    }
+}
