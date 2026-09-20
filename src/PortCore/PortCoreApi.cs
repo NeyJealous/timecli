54c7b3101d2ca195af91dadd0c9362c450af7ac9
NeyJealous <rismahanov@yandex.ru>
2026-09-20 07:08:32 +0300

    chore: bump PortCore API for exact cube pickup lifecycle

--- src/PortCore/PortCoreApi.cs
+++ src/PortCore/PortCoreApi.cs
@@ -7,8 +7,8 @@ namespace TimeClickers.PortCore;
 public static class PortCoreApi
 {
     public const int Major = 1;
-    public const int Minor = 0;
+    public const int Minor = 1;
     public const int Patch = 0;
-    public const string Version = "1.0.0";
+    public const string Version = "1.1.0";
     public const bool Phase2Frozen = true;
 }
