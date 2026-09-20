14782c0fcd8003bebbb12e04a202baf799404584
NeyJealous <rismahanov@yandex.ru>
2026-09-20 03:18:37 +0300

    fix: qualify Unity random source for adapter preflight

--- unity/TimeCli/Assets/TimeCli/Scripts/Runtime/UnityRandomSource.cs
+++ unity/TimeCli/Assets/TimeCli/Scripts/Runtime/UnityRandomSource.cs
@@ -7,16 +7,16 @@ namespace TimeCli.UnityRuntime
     {
         public int Range(int minInclusive, int maxExclusive)
         {
-            return Random.Range(minInclusive, maxExclusive);
+            return UnityEngine.Random.Range(minInclusive, maxExclusive);
         }
 
         public static ArenaSpawnRolls NextSpawnRolls()
         {
             return new ArenaSpawnRolls(
-                ModelRandomIndex: Random.Range(0, 1_000_000),
-                RainbowEnemyRoll: Random.value,
-                TimeCubeRoll: Random.value,
-                WeaponCubeRoll: Random.value);
+                ModelRandomIndex: UnityEngine.Random.Range(0, 1_000_000),
+                RainbowEnemyRoll: UnityEngine.Random.value,
+                TimeCubeRoll: UnityEngine.Random.value,
+                WeaponCubeRoll: UnityEngine.Random.value);
         }
     }
 }
